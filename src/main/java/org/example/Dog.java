c9c130b227bb9595c198fd4c1f4880e3ccf7ffeb
mahadeva-shindhe <mahadeva.shindhe@gds.ey.com>
2022-11-01 20:51:28 +0530

    Factory Design Pattern example

---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Dog implements Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating...");
+    }
+}
---
+++ src/main/java/org/example/Tiger.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Tiger implements Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("Tiger is eating...");
+    }
+}
